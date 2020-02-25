9deb034e88d7b1b1e4d281d6550bfbac7f4e6c04
koriavinash1 <koriavinash1@gmail.com>
2020-02-25 11:46:40 +0530

    setup fix

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
 import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='pgm',  
+     version='0.0.1',
+     author="Avinash Kori",
+     author_email="koriavinash1@gmail.com",
+     description="Graphical model analysis toolbox.",
+     long_description=open("README.md").read(),
+   long_description_content_type="text/markdown",
+     url="https://github.com/koriavinash1/pgm",
+     packages=setuptools.find_packages(),
+     install_requires = [
+         'pandas',
+	 'numpy'
+         ],
+     classifiers=[
+         "Programming Language :: Python :: <=3.5",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
