0e6dba3937aa5504b294c17cafc4d89603ed1e9a
koriavinash1 <koriavinash1@gmail.com>
2020-03-14 23:28:54 +0530

    gibbs sampling

--- pgm/inference/Gibbs.py
+++ pgm/inference/Gibbs.py
@@ -14,9 +14,9 @@ class GibbsSampler(object):
         # sample x
         self.x = np.round(np.random.uniform(0, 1, \
                         (self.Xnodes, self.Ynodes)))
-        
+        self.count_seq = []
         self.x_seq = []
-
+        
     def getConditional(self, x, i, j):
         """
         """
@@ -42,10 +42,10 @@ class GibbsSampler(object):
             (ii < 0) or (jj  < 0) or self.visited[ii,jj]:
             return x_next
         
+        self.count_seq.append(self.count(x_next))
         # get p based on conditional
         p = self.getConditional(x_next, ii, jj)
         
-        # print (ii,jj, p)
         # sample xij
         x_next[ii, jj] = x_next[ii, jj] if p > np.random.uniform(0,1) else 1 - x_next[ii, jj]
 
@@ -63,8 +63,29 @@ class GibbsSampler(object):
         self.visited = np.zeros_like(self.x)
         x_next = np.copy(self.x)
         x_next = self.localSampler(x_next, ii, jj)
-
         return x_next
+
+    def countUtils(self, x, c, i, j):
+        """
+        """
+        if (i + 1  >= self.x.shape[0]) or\
+            (j + 1 >= self.x.shape[1]) or \
+            (i < 0) or (j  < 0) or (self.vis[i,j] == 1):
+            return c
+
+        self.vis[i,j] = 1
+        if x[i+1, j] == 1.:
+            c += self.countUtils(x, c+1, i+1, j)
+        elif x[i, j+1] == 1.:
+            c += self.countUtils(x, c+1, i, j+1)
+
+        return c
+
+    def count(self, x):
+        """
+        """
+        self.vis = np.zeros_like(x)
+        return self.countUtils(x, 0, 0, 0) 
     
     def sampler(self):
         """
@@ -73,7 +94,7 @@ class GibbsSampler(object):
             for i in range(self.burninT):
                 ii = np.random.randint(0, self.Xnodes)
                 jj = np.random.randint(0, self.Ynodes)
-                x_next = self.Proposal(0, 0)
+                x_next = self.Proposal(ii, jj)
                 self.x = x_next
             self.x_seq.append(self.x)
             yield self.x
@@ -87,12 +108,19 @@ if __name__ == '__main__':
                           (0,0): 1,
                           (1,0): 1,
                           (0,1): 1}
-    gs = GibbsSampler(local_distribution, 1, 24, 24)
+    gs = GibbsSampler(local_distribution, 1, 16, 16)
     
-    epochs = 1000
+    epochs = 1
     for i in range(epochs):
         x = next(gs.sampler())
-        if i > 995:
-            plt.imshow(x)
-            plt.show()
+        plt.clf()
+        plt.imshow(x)
+        plt.show()
+
+    plt.clf()
+    plt.plot(gs.count_seq)
+    plt.xlabel("mixture progression")
+    plt.ylabel("count")
+    plt.title("Epoch: {}".format(i))
+    plt.show()
 
