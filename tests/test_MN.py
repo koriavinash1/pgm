06c2e5b5cd06a08207d87b7de6d22f86fb0d4bcf
koriavinash1 <koriavinash1@gmail.com>
2020-04-25 12:42:44 +0530

    MN: tests

---
+++ tests/test_MN.py
@@ -0,0 +1,40 @@
+import unittest
+from pgm.helpers.common import Node
+from pgm.helpers.search import dfs
+from pgm.representation.LinkedListMN import Graph
+
+
+class TestGraphMethods(unittest.TestCase):
+
+    def test_node_addition(self):
+        rootNode = Node('rootNode')
+        graph = Graph(rootNode)
+        graph.add_node('node1', 'rootNode')
+        graph.add_node('node2', 'rootNode')
+        self.assertEqual(len(rootNode.nbrs), 2)
+
+
+    def test_search_node(self):
+        rootNode = Node('rootNode')
+        graph = Graph(rootNode)
+        graph.add_node('node1', 'rootNode')
+        graph.add_node('node2', 'rootNode')
+
+        node1 = dfs(rootNode, 'node1').searchNode
+        node2 = dfs(rootNode, 'node2').searchNode
+        self.assertEqual(node1.name, 'node1')
+        self.assertEqual(node2.name, 'node2')
+
+
+    def test_edge_addition(self):
+        rootNode = Node('rootNode')
+        graph = Graph(rootNode)
+        graph.add_node('node1', 'rootNode')
+        graph.add_node('node2', 'rootNode')
+        graph.add_edge('node2', 'node1')
+        node1 = dfs(rootNode, 'node1').searchNode
+        self.assertEqual(node1.name, 'node1')
+        self.assertEqual(len(node1.nbrs), 2)
+
+if __name__ == '__main__':
+    unittest.main()
