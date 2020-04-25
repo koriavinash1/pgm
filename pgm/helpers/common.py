491f6cfd26efb5a620273d0d0a8f4dd72c1b8aca
koriavinash1 <koriavinash1@gmail.com>
2020-04-25 11:40:21 +0530

    tests

--- pgm/helpers/common.py
+++ pgm/helpers/common.py
@@ -1,10 +1,16 @@
 import numpy as np
 
 class Node(object):
-    """
+    r"""
+    Commom Node defn for both BN and MN
+
+    Includes ndr attribute for MN
+    and includes parents and children for BN
+    values for {set of values that node 
+    distribution takes}
     """
     def __init__(self, name=None):
-        
+            
         self.name = name
         self.parents  = []
         self.children = []
---
+++ tests/test_BN.py
@@ -0,0 +1,47 @@
+import unittest
+from pgm.helpers.common import Node
+from pgm.helpers.search import dfs
+from pgm.representation.LinkedListBN import Graph
+
+
+graph.add_node('node1', 'rootNode')
+graph.add_node('node2', 'rootNode')
+graph.add_node('node3', 'node1')
+graph.add_edge('node2', 'node3')
+graph.print(rootNode)
+
+
+class TestGraphMethods(unittest.TestCase):
+
+    def test_node_addition(self):
+        rootNode = Node('rootNode')
+        graph = Graph(rootNode)
+        graph.add_node('node1', 'rootNode')
+        graph.add_node('node2', 'rootNode')
+        self.assertEqual(len(rootNode.children), 2)
+
+
+    def test_search_node(self):
+        rootNode = Node('rootNode')
+        graph = Graph(rootNode)
+        graph.add_node('node1', 'rootNode')
+        graph.add_node('node2', 'rootNode')
+
+        node1 = dfs(rootNode, 'node1').searchNode
+        node2 = dfs(rootNode, 'node2').searchNode
+        self.assertEqual(node1.name, 'node1')
+        self.assertEqual(node2.name, 'node2')
+
+
+    def test_edge_addition(self):
+        rootNode = Node('rootNode')
+        graph = Graph(rootNode)
+        graph.add_node('node1', 'rootNode')
+        graph.add_node('node2', 'rootNode')
+        graph.add_edge('node2', 'node1')
+        node2 = dfs(rootNode, 'node2').searchNode
+        self.assertEqual(node2.name, 'node2')
+        self.assertEqual(len(node2.parents), 2)
+
+if __name__ == '__main__':
+    unittest.main()
