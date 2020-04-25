e8cafeb37da5f358437cd472a9b62d1934755405
koriavinash1 <koriavinash1@gmail.com>
2020-04-25 12:52:14 +0530

    MN: tests

--- tests/test_MN.py
+++ tests/test_MN.py
@@ -20,8 +20,8 @@ class TestGraphMethods(unittest.TestCase):
         graph.add_node('node1', ['rootNode'])
         graph.add_node('node2', ['rootNode'])
 
-        node1 = dfs(rootNode, 'node1').searchNode
-        node2 = dfs(rootNode, 'node2').searchNode
+        node1 = dfs(rootNode, 'node1', type = 'MN').searchNode
+        node2 = dfs(rootNode, 'node2', type = 'MN').searchNode
         self.assertEqual(node1.name, 'node1')
         self.assertEqual(node2.name, 'node2')
 
@@ -32,7 +32,7 @@ class TestGraphMethods(unittest.TestCase):
         graph.add_node('node1', ['rootNode'])
         graph.add_node('node2', ['rootNode'])
         graph.add_edge('node2', 'node1')
-        node1 = dfs(rootNode, 'node1').searchNode
+        node1 = dfs(rootNode, 'node1', type = 'MN').searchNode
         self.assertEqual(node1.name, 'node1')
         self.assertEqual(len(node1.nbrs), 2)
 
