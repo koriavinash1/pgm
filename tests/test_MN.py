ad5a60ac784209227db82bd555247e37bfdc9118
koriavinash1 <koriavinash1@gmail.com>
2020-04-25 12:47:43 +0530

    MN: tests

--- tests/test_MN.py
+++ tests/test_MN.py
@@ -9,16 +9,16 @@ class TestGraphMethods(unittest.TestCase):
     def test_node_addition(self):
         rootNode = Node('rootNode')
         graph = Graph(rootNode)
-        graph.add_node('node1', 'rootNode')
-        graph.add_node('node2', 'rootNode')
+        graph.add_node('node1', ['rootNode'])
+        graph.add_node('node2', ['rootNode'])
         self.assertEqual(len(rootNode.nbrs), 2)
 
 
     def test_search_node(self):
         rootNode = Node('rootNode')
         graph = Graph(rootNode)
-        graph.add_node('node1', 'rootNode')
-        graph.add_node('node2', 'rootNode')
+        graph.add_node('node1', ['rootNode'])
+        graph.add_node('node2', ['rootNode'])
 
         node1 = dfs(rootNode, 'node1').searchNode
         node2 = dfs(rootNode, 'node2').searchNode
@@ -29,8 +29,8 @@ class TestGraphMethods(unittest.TestCase):
     def test_edge_addition(self):
         rootNode = Node('rootNode')
         graph = Graph(rootNode)
-        graph.add_node('node1', 'rootNode')
-        graph.add_node('node2', 'rootNode')
+        graph.add_node('node1', ['rootNode'])
+        graph.add_node('node2', ['rootNode'])
         graph.add_edge('node2', 'node1')
         node1 = dfs(rootNode, 'node1').searchNode
         self.assertEqual(node1.name, 'node1')
