d58ae405166325b06c0a21954744d14375767943
koriavinash1 <koriavinash1@gmail.com>
2020-05-01 10:32:55 +0530

    random graph skeleton

--- pgm/inference/messageParsing.py
+++ pgm/inference/messageParsing.py
@@ -9,34 +9,34 @@ class MessageParsing(object):
     """
 
     def __init__(self, rootNode, type='BN'):
-    	r"""
+        r"""
 
-    	rootNode: Node attribute
-    	type : 'BN' or 'MN'
-    	"""
-    	self.type = type
-    	if isinstance(root, Node):
+        rootNode: Node attribute
+        type : 'BN' or 'MN'
+        """
+        self.type = type
+        if isinstance(root, Node):
             self.rootNode = root
 
     def parse_message(self, node1, node2):
-    	r""" Caclulates the message from node1 -> node2
-		message_{node1->node2} = 
-			\sum_{node1} \phi(node1)\phi(node1, node2)
-				\prod_{pa \in parents_{node1}} message_{pa->node1}
+        r""" Caclulates the message from node1 -> node2
+        message_{node1->node2} = 
+            \sum_{node1} \phi(node1)\phi(node1, node2)
+                \prod_{pa \in parents_{node1}} message_{pa->node1}
 
-		node1: ['int' or 'str'] or <Node> object
-		node2: ['int' or 'str'] or <Node> object
-    	"""
+        node1: ['int' or 'str'] or <Node> object
+        node2: ['int' or 'str'] or <Node> object
+        """
 
-		if not isinstance(node1, Node):
-			node1 = dfs(self.rootNode, node1, self.type).searchNode
+        if not isinstance(node1, Node):
+            node1 = dfs(self.rootNode, node1, self.type).searchNode
 
         if not isinstance(node2, Node):
-			node2 = dfs(self.rootNode, node2, self.type).searchNode
+            node2 = dfs(self.rootNode, node2, self.type).searchNode
 
-		if self.type=='BN':
-			assert node2 in node1.children, "No direct link between node1 and node2"
-		else:
-			assert node2 in node1.nbrs, "No direct link between node1 and node2"
+        if self.type=='BN':
+            assert node2 in node1.children, "No direct link between node1 and node2"
+        else:
+            assert node2 in node1.nbrs, "No direct link between node1 and node2"
 
-		
+        
